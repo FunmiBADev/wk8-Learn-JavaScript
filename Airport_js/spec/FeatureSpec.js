a847d74e45920fe1d75e273ae727b6a20b04d57c
FunmiBADev <funmiba@hotmail.com>
2020-03-24 00:04:12 +0000

    Feature, Aiport, Plane spec test and part completed src Airport/Plane.js - One error still pending Expected [  ] to contain Plane({  }).

---
+++ Airport_js/spec/FeatureSpec.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+describe('Feature Test:', function(){
+	var plane;
+	var airport;
+	beforeEach(function(){
+		plane = new Plane();
+		airport = new Airport();
+	});
+// As an air traffic controller 
+// To get passengers to a destination 
+// I want to instruct a plan to land at 
+//  an airport and confirm that it has landed
+	it('planes can be instructed to land at an airport', function(){
+		plane.land(airport);
+		expect(airport.planes()).toContain(plane);
+	});
+});
