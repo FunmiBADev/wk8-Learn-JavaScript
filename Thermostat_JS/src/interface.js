da1d78ab7a39218942c8e752333b6dbe21f90d89
FunmiBADev <funmiba@hotmail.com>
2020-03-26 22:01:05 +0000

    introduced thermostat APIs

--- Thermostat_JS/src/interface.js
+++ Thermostat_JS/src/interface.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
 	var thermostat = new Thermostat();
 	updateTemperature;
 
+	displayWeather('London');
+	$('#select-city').submit(function(event){
+		event.preventDefault();
+		var city = $('#current-city').val();
+		displayWeather(city);
+	});
+
+
 	$('#temperature-up').on('click', function(){
 		thermostat.up();
 		updateTemperature;
@@ -31,5 +39,14 @@ $(document).ready(function(){
 	function updateTemperature(){
 		$('#temperature').text(thermostat.temperature);
 		$('#temperature').attr('class', thermostat.energyUsage());
+	};
+
+	function displayWeather(city) {
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+		var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+		var units = '&units=metric';
+		$.get(url + token + unit, function(){
+			$('#current-temperature').text(data.main.temp);
+		});
 	};
 });
